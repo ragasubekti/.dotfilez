83cad62b9d48afddb49d7f4a56d54aa3b9e72b30
Raga Subekti <ragasubekti@gmail.com>
2021-12-04 02:00:29 +0700

    fix: remove confirmation on pacman

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ export SHELL=/bin/zsh
 DISTRO=$(awk -F '=' '/^ID=/ {print tolower($2)}' /etc/*-release)
 
 if [[ $DISTRO -eq "arch" ]]; then
-    sudo pacman -Syu aria2 unzip git base-devel zsh tmux curl
+    sudo pacman -Syu --noconfirm aria2 unzip git base-devel zsh tmux curl
 elif [[ $DISTRO -eq "ubuntu" ]]; then
     sudo apt install build-essentials git aria2 unzip zsh tmux curl
 fi
