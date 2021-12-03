05d469986439e518c210c89b971aa9bcc4063db5
Raga Subekti <ragasubekti@gmail.com>
2021-12-04 02:25:56 +0700

    fix: automate android licenses

--- .setup/android.sh
+++ .setup/android.sh
@@ -23,5 +23,5 @@ fi
 
 source $HOME/.zshrc
 # sdkmanager "platform-tools"
-sdkmanager "platforms;android-29" "build-tools;29.0.3" "platform-tools"
-sdkmanager --licenses
+yes | sdkmanager "platforms;android-29" "build-tools;29.0.3" "platform-tools"
+yes | sdkmanager --licenses
